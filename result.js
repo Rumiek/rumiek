29a33e17a4aa5f036705edd250da4add514b2178
James Halliday <91103560+Rumiek@users.noreply.github.com>
2021-12-28 00:37:56 -0400

    initial commit

--- result.js
+++ result.js
@@ -36,72 +36,73 @@ function result(s){
   if(this.readyState == 4 && this.status == 200){
 
     const res = JSON.parse(this.response);
-    let item = '';
+    //let item = '';
+    let thumbnails = [];
+    let id = [];
     res.items.forEach(function(pictures){
 
-      let thumbnails = pictures.snippet.thumbnails.default.url;
-      let title = pictures.snippet.title;
-      let id = pictures.id.videoId;
-      item =item + `<div class="music-file" data-media="9354e95eb77661626e5a392d6578376f">
-            <div class="mf-image">
-              <div class="mf-ibg mcover"></div>
-              <img class="lazy-thumbs" src="${thumbnails}" data-title="${title}"/>
-            </div>
-            <div class="mf-details">
-              <div class="mf-content">
-                <h3>${title}</h3>
-                <!--<ul class="mf-info">
-                  <li>
-                    <span class="fas fa-user-crown"></span>Nirvana
-                  </li>
-                  <li>
-                    <span class="far fa-calendar-star"></span>12 years ago
-                  </li>
-                </ul>
-                <div class="mf-desc"> As heard in Captain Marvel! Listen to more Nirvana here: Nirvana.lnk.to/Essentials Read the story behind 'Nevermind' here: ... </div>-->
-                <div class="music-player"></div>
-              </div>
-              <ul class="mf-actions">
-                <li data-mact="pm">
-                  <span class="icon">
-                    <span class="fas fa-play fa-fw"></span>
-                    <span class="fas fa-spinner-third fa-spin fa-fw"></span>
-                  </span>
-                  <a>
-                    <span class="text">
-                      <span class="large">Play Music</span>
-                      <span class="short">Play</span>
-                    </span>
-                  </a>
-                </li>
-                <li data-mact="sm">
-                  <span class="icon">
-                    <span class="fas fa-circle fa-fw"></span>
-                  </span>
-                  <a>
-                    <span class="text">
-                      <span class="large">Stop Music</span>
-                      <span class="short">Stop</span>
-                    </span>
-                  </a>
-                </li>
-                <li data-mact="dl">
-                  <span class="icon">
-                    <span class="fas fa-download fa-fw"></span>
-                  </span>
-                  <a href="https://mp3snow.com/${id}" target="_blank">
-                      <span class="text">
-                        <span class="large">Download MP3</span>
-                        <span class="short">Download</span>
-                    </span>
-                  </a>
-                </li>
-              </ul>
-            </div>
-          </div>`;
+      //title.push(pictures.snippet.title);
+      thumbnails.push(`<div class="mf-image">
+        <div class="mf-ibg mcover"></div>
+        <img class="lazy-thumbs" src="${pictures.snippet.thumbnails.default.url}" data-title="${pictures.snippet.title}"/>
+      </div>`);
+      id.push(`            <div class="mf-details">
+                    <div class="mf-content">
+                      <h3>${pictures.snippet.title}</h3>
+                      <!--<ul class="mf-info">
+                        <li>
+                          <span class="fas fa-user-crown"></span>Nirvana
+                        </li>
+                        <li>
+                          <span class="far fa-calendar-star"></span>12 years ago
+                        </li>
+                      </ul>
+                      <div class="mf-desc"> As heard in Captain Marvel! Listen to more Nirvana here: Nirvana.lnk.to/Essentials Read the story behind 'Nevermind' here: ... </div>-->
+                      <div class="music-player"></div>
+                    </div>
+                    <ul class="mf-actions">
+                      <li data-mact="pm">
+                        <span class="icon">
+                          <span class="fas fa-play fa-fw"></span>
+                          <span class="fas fa-spinner-third fa-spin fa-fw"></span>
+                        </span>
+                        <a>
+                          <span class="text">
+                            <span class="large">Play Music</span>
+                            <span class="short">Play</span>
+                          </span>
+                        </a>
+                      </li>
+                      <li data-mact="sm">
+                        <span class="icon">
+                          <span class="fas fa-circle fa-fw"></span>
+                        </span>
+                        <a>
+                          <span class="text">
+                            <span class="large">Stop Music</span>
+                            <span class="short">Stop</span>
+                          </span>
+                        </a>
+                      </li>
+                      <li data-mact="dl">
+                        <span class="icon">
+                          <span class="fas fa-download fa-fw"></span>
+                        </span>
+                        <a href="https://mp3snow.com/${pictures.id.videoId}" target="_blank">
+                            <span class="text">
+                              <span class="large">Download MP3</span>
+                              <span class="short">Download</span>
+                          </span>
+                        </a>
+                      </li>
+                    </ul>
+                  </div>
+                </div>`);
         }
     );
-    document.querySelector('.music-files').innerHTML = `${item}`;
+    //console.log(thumbnails);
+    //console.log(id);
+    document.querySelector('.music-files').innerHTML = `${music_file(thumbnails, id)}`;
   }
 };
 
@@ -125,5 +126,11 @@ function titles(){
 
   app.innerHTML = `${text}`;
   subtitle.innerHTML = `${text} Songs`;
-
+}
+function music_file(x, y){
+  let container = '';
+  for (var i = 0; i < x.length; i++) {
+    container +=`<div class="music-file" data-media="9354e95eb77661626e5a392d6578376f">${x[i]+y[i]}</div>`;
+  }
+  return container;
 }
